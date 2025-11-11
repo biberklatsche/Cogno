db7357635cb04c92c0ea2919c359a31adb093332
Lars <lars.wolfram@devlab.expert>
2025-11-11 08:22:30 +0100

    Adds window menu service
    Adds a service to handle the creation of the application window menu, specifically tailored for macOS.
    
    The menu includes "About" and "File" submenus, with corresponding menu items and actions.
    
    Configures accelerators for menu items and sets the menu as the application menu.

--- src/app/menu/window-menu.service.ts
+++ src/app/menu/window-menu.service.ts
@@ -3,25 +3,51 @@ import {OS} from "../_tauri/os";
 import {Submenu} from "@tauri-apps/api/menu/submenu";
 import {MenuItem} from "@tauri-apps/api/menu/menuItem";
 import {Menu} from "@tauri-apps/api/menu/menu";
+import {ConfigService} from "../config/+state/config.service";
+import {PhysicalPosition} from "@tauri-apps/api/dpi";
+import {PredefinedMenuItem} from "@tauri-apps/api/menu/predefinedMenuItem";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WindowMenuService {
+
+  private _menu: Menu | undefined;
+
   constructor() {
       switch (OS.platform()) {
           case 'windows':
           case "linux":
               break;
           case 'macos':
-              this.createMacOsMenu().then();
+              this.createMacOsMenu('Shift+Command+Control+Alt+A').then();
       }
+
+      setTimeout(async () => {
+          const cogno = await this._menu!.get('cogno') as Submenu;
+          const about = await cogno.get('about') as MenuItem;
+          await about.setAccelerator('Ctrl+Alt+A');
+          await this._menu?.setAsAppMenu();
+      }, 5000);
   }
 
-  private async createMacOsMenu() {
+  private async createMacOsMenu(accelerator: string) {
+
       const aboutSubmenu = await Submenu.new({
-          text: 'About',
+          id: 'cogno',
+          text: 'Cogno',
           items: [
+              await MenuItem.new({
+                  id: 'about',
+                  text: 'About',
+                  accelerator: accelerator,
+                  action: () => {
+                      console.log('About pressed');
+                  },
+              }),
+              await PredefinedMenuItem.new({
+                  item: 'Separator',
+              }),
               await MenuItem.new({
                   id: 'quit',
                   text: 'Quit',
@@ -33,10 +59,24 @@ export class WindowMenuService {
           ],
       });
 
-      const menu = await Menu.new({
-          items: [aboutSubmenu],
+      const fileSubmenu = await Submenu.new({
+          text: 'File',
+          items: [
+              await MenuItem.new({
+                  id: 'copy',
+                  text: 'Copy',
+                  accelerator: 'Shift+Command+Control+Alt+A',
+                  action: () => {
+                      console.log('Quit pressed');
+                  },
+              }),
+          ],
       });
 
-      await menu.setAsAppMenu();
+      this._menu = await Menu.new({
+          items: [aboutSubmenu, fileSubmenu],
+      });
+
+      await this._menu.setAsAppMenu();
   }
 }
