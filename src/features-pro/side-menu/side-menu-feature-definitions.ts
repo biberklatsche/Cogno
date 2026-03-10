5f8a29cb77d0ab050e02f33c9fae4c76f60cb21c
Lars Wolfram <lars.wolfram@devlab.expert>
2026-03-10 14:41:29 +0100

    disable excample side menu

--- src/features-pro/side-menu/side-menu-feature-definitions.ts
+++ src/features-pro/side-menu/side-menu-feature-definitions.ts
@@ -2,6 +2,6 @@ import { Type } from "@angular/core";
 import { SideMenuFeatureDefinitionContract } from "@cogno/core-sdk";
 import { proExampleSideMenuFeatureDefinition } from "./pro-example/pro-example.feature-definition";
 
-export const proFeatureSideMenuFeatureDefinitions = [proExampleSideMenuFeatureDefinition] as const satisfies ReadonlyArray<
+export const proFeatureSideMenuFeatureDefinitions = [] as const satisfies ReadonlyArray< //[proExampleSideMenuFeatureDefinition] as const satisfies ReadonlyArray<
   SideMenuFeatureDefinitionContract<Type<unknown>, string, string>
 >;
